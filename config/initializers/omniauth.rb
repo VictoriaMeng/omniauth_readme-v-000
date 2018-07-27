88d9f5a665648b50416caf07dea58926d108018b
Victoria Meng <nights1ststar@protonmail.com>
2018-07-27 00:33:06 +0000

    automatically backed up by learn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> 866e9bf5ff275fe9ff76931efce5555e9755df49
